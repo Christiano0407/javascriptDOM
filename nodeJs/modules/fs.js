91a31b6b35abab59aac1a14952c04fff3cdd7f9a
Christiano0407 <placebo0704@gmail.com>
2022-03-24 23:26:28 -0600

    fylseSystem 02

--- nodeJs/modules/fs.js
+++ nodeJs/modules/fs.js
@@ -3,9 +3,10 @@ const fs = require("fs");
 
 function leer(rute, callback) {
   fs.readFile(rute, (err, data) => {
-    console.log(data.toString());
+    //console.log(data.toString());
+    callback(data.toString()); //** => Viene siendo el Console.log */
   });
 }
 
-leer(__dirname + "/fstext.txt");
+leer(__dirname + "/fstext.txt", console.log);
 //**==> Le decimos que accesa a ese archivo y transforme en texto la data == */
